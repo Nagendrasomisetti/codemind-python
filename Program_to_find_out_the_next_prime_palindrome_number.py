ef5da75d0061bbaac4743e8d0343e88926b93183
Nagendrasomisetti <119730093+Nagendrasomisetti@users.noreply.github.com>
2023-05-22 21:02:00 +0530

    def prime(j):     i=2     v=0     while i!=j:         if j%i==0:             v=1         i+=1     if v==0:         return j def is_palin(j):     m = j     s=0     while m!=0:         v = m%10         s = s*10+v         m = m//10     if j==s:         return j x = int(input()) g = x+1 while g!=0:     if is_palin(g):         if prime(g):             print(g)             break     g+=1

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,26 @@
+def prime(j):
+    i=2
+    v=0
+    while i!=j:
+        if j%i==0:
+            v=1
+        i+=1 
+    if v==0:
+        return j
+def is_palin(j):
+    m = j
+    s=0
+    while m!=0:
+        v = m%10 
+        s = s*10+v 
+        m = m//10
+    if j==s: 
+        return j
+x = int(input())
+g = x+1
+while g!=0:
+    if is_palin(g):
+        if prime(g):
+            print(g)
+            break
+    g+=1
